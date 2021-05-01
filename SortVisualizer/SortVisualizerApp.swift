205aa431e60cb2e0080e9fa8b59f2fef96236d70
TiagoMaiaL <tiago.maia.lopes@gmail.com>
2021-04-30 22:10:03 -0300

    Add initial Mac app Xcode project

---
+++ SortVisualizer/SortVisualizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SortVisualizerApp.swift
+//  SortVisualizer
+//
+//  Created by Tiago Lopes on 30/04/21.
+//
+
+import SwiftUI
+
+@main
+struct SortVisualizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
